acc733f1f83936ba5a6cda6af494b9dcf96b1e87
Deep-Parekh <dparekh20@outlook.com>
2021-05-06 23:10:18 -0700

    Added logic to add anotehr canvas element with ascii encoded image

--- action.js
+++ action.js
@@ -1,8 +1,9 @@
 
 const canvas = document.getElementById("preview");
 const fileInput = document.querySelector('input[type="file"');
-
+const buttonInput = document.querySelector('button');
 const context = canvas.getContext("2d");
+var image;
 
 fileInput.onchange = e => {
   // just handling single file upload
@@ -10,7 +11,7 @@ fileInput.onchange = e => {
 
   const reader = new FileReader();
   reader.onload = event => {
-    const image = new Image();
+    image = new Image();
     image.onload = () => {
       canvas.width = image.width;
       canvas.height = image.height;
@@ -23,3 +24,25 @@ fileInput.onchange = e => {
 
   reader.readAsDataURL(file);
 };
+
+buttonInput.onclick = e => {
+  console.log('Clicked convert button')
+  let myPixelArt = '';
+  // Start the first row of "pixels".
+  myPixelArt += '<div class="a-row-of-pixels">';
+  for (let y = 0; y < image.height; y++) {
+    for (let x = 0; x < image.width; x++) {
+      const theImageData = context.getImageData(x, y, 1, 1);
+      const theRGBvalues = theImageData.data;
+
+      // Concatenate every column of "pixels" in this row, one after the other.
+      myPixelArt += `<div class="a-pixel" style="background: rgb(${ theRGBvalues[0] }, ${ theRGBvalues[1] }, ${ theRGBvalues[2] })"></div>`;
+
+    }
+    // Concatenate the end of the row and the beginning of a new one.   
+    myPixelArt += '</div><div class="a-row-of-pixels">';
+  }
+  // The last row will be empty but who cares, let's close it.
+  myPixelArt += '</div>';
+  document.getElementById("ascii-art").innerHTML += myPixelArt;
+}
